4a7c34053318abd563809fb227eff72f1a775870
hardiksuthar74 <76840141+hardiksuthar74@users.noreply.github.com>
2023-05-19 15:12:28 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -53,7 +53,6 @@ const sectionHeroEl = document.querySelector(".section-hero");
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
-    console.log(ent);
 
     if (ent.isIntersecting === false) {
       document.body.classList.add("sticky");
